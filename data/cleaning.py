a9b552873a93f7031a69cfe2ce5b307b382c8602
karimtaaam <karim.taaam@qodash.com>
2020-03-17 11:56:06 -0400

    update_positions

--- data/cleaning.py
+++ data/cleaning.py
@@ -38,7 +38,7 @@ def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
-def clean_positions(max_positions_split=1000):
+def clean_positions(max_positions_split=100):
     filings=db['filings_13f']
     res=filings.find({},batch_size=1000)
     bar=tqdm(res,desc='filings',total=res.count())
