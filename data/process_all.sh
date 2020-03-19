64a941f5c04a20add13f7a5cdd04fb7c8c2e91c6
karimtaaam <karim.taaam@qodash.com>
2020-03-18 22:18:39 -0400

    update_positions

--- data/process_all.sh
+++ data/process_all.sh
@@ -9,20 +9,23 @@ print_title () {
 eval "$(conda shell.bash hook)"
 conda activate edgar
 echo "python=$(which python)"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export PYTHONPATH=$(dirname $DIR)
+echo $PYTHONPATH
 
-print_title "Creating database views"
-python views.py
-
-print_title "Cleaning filing indexes"
-python cleaning.py index
-
-print_title "Cleaning filing stock info"
-python cleaning.py info
-
-print_title "Cleaning filing positions"
-python cleaning.py positions
-
-print_title "Building positions collection"
-cores=$(($(nproc)-2))
-echo "Using number of cores=$cores"
-python update_positions.py --n_cores=$cores run
+#print_title "Creating database views"
+#python views.py
+#
+#print_title "Cleaning filing indexes"
+#python cleaning.py index
+#
+#print_title "Cleaning filing stock info"
+#python cleaning.py info
+#
+#print_title "Cleaning filing positions"
+#python cleaning.py positions
+#
+#print_title "Building positions collection"
+#cores=$(($(nproc)-2))
+#echo "Using number of cores=$cores"
+#python update_positions.py --n_cores=$cores run
