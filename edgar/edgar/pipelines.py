e022dbe5d57887b3abc00a676022fd5578cfbdc9
karimtaaam <karim.taaam@qodash.com>
2020-03-22 11:13:37 -0400

    remove pycache

--- edgar/edgar/pipelines.py
+++ edgar/edgar/pipelines.py
@@ -89,6 +89,7 @@ class EdgarPipeline(object):
 
 
 class PositionsPipeline(EdgarPipeline):
+    @profile
     def process_item(self, item, spider):
         self.spider=spider
         ## how to handle each filing
