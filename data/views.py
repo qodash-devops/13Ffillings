57e27c101c7d53a9502fce545f65e553ff0d6e63
karimtaaam <karim.taaam@qodash.com>
2020-03-25 10:36:45 -0400

    remove pycache

--- data/views.py
+++ data/views.py
@@ -2,11 +2,18 @@ from fire import Fire
 from edgar.edgar.settings import MONGO_DATABASE, MONGO_URI
 import logging
 import pymongo
+import colorlog
 import sys
 client = pymongo.MongoClient(MONGO_URI)
 db = client[MONGO_DATABASE]
 stock_info = db['stock_info']
 filings = db['filings_13f']
+
+
+logger = logging.getLogger('item_worker')
+handler = colorlog.StreamHandler()
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 
 def create_view(n, p, collection_on="filings_13f"):
@@ -28,46 +35,33 @@ views = {
                             {"$group": {"_id": "$filer_name", "filed_quarters": {"$addToSet": "$quarter"}}},
                             {"$lookup":{"from":"all_quarters_view","foreignField":"all_quarters_view","localField":"all_quarters","as":"all_quarters"}},
                             {"$project":{"filer_name":"$filer_name","filed_quarters":"$filed_quarters","missing_quarters":{"$setDifference":[{"$arrayElemAt":["$all_quarters.quarters",0]},"$filed_quarters"]}}}],
-
-    "positions_view":[{"$unwind": "$positions"},{"$project": {"filer_name":1,"quarter_date":1,"cusip":"$positions.cusip","quantity":"$positions.quantity"}}],
-    "positions_stock":[ {"$unwind": "$positions"},{"$project": {"filer_name":1,"quarter_date":1,"cusip":"$positions.cusip","quantity":"$positions.quantity"}},
-                        {"$lookup": {"from": "stock_info","localField": "cusip","foreignField": "cusip","as": "stock_info"}},
-                        {"$project": {"quarter_date":{"$dateFromString": {"dateString": "$quarter_date","format":"%m-%d-%Y"}},
-                                    "filer_name":1,"quantity":{"$toDouble": "$quantity"},
-                                    "dates":{"$map": {"input": {"$arrayElemAt": ["$stock_info.close.Date",0]},"as": "itemdate","in": {"$dateFromString": {"dateString": "$$itemdate","format":"%Y-%m-%d"}}}},
-                                    "prices":{"$arrayElemAt": ["$stock_info.close",0]},
-                                    "ticker":{"$arrayElemAt": ["$stock_info.ticker",0]},
-                                    "market_cap":{"$arrayElemAt": ["$stock_info.info.marketCap",0]},
-                                    "sector":{"$arrayElemAt": ["$stock_info.info.sector",0]},
-                                    "volume":{"$arrayElemAt": ["$stock_info.info.regularMarketVolume",0]}}},
-                        {"$project": {"quarter_date":1,"quantity":1,"filer_name":1,"init_date":{"$min":{"$filter": {"input": "$dates","as": "item","cond": {"$gte": ["$$item","$quarter_date"]}}}},
-                                    "past_q_date":{"$add":["$quarter_date",- 1000 * 3600 * 24 * 30.5 * 3]},"next_q_date":{"$add":["$quarter_date", 1000 * 3600 * 24 * 30.5 * 3]},
-                                    "dates":1,"prices":1,"ticker":1,"market_cap":1,"sector":1,"volume":1}},
-                        {"$project": {"init_date":1,"quarter_date":1,"quantity":1,"filer_name":1,"prices":1,"ticker":1,"market_cap":1,"sector":1,"volume":1,
-                                    "past_q_date":{"$min":{"$filter": {"input": "$dates","as": "item","cond": {"$gte": ["$$item","$past_q_date"]}}}},
-                                    "next_q_date":{"$min":{"$filter": {"input": "$dates","as": "item","cond": {"$gte": ["$$item","$next_q_date"]}}}}
-                        }},
-                        {"$project": {"init_date":1,"quarter_date":1,"quantity":1,"filer_name":1,"prices":1,"ticker":1,"market_cap":1,"sector":1,"volume":1,"past_q_date":1,"next_q_date":1,
-                                   "init_spot":{"$arrayElemAt": [{"$filter": {"input": "$prices","as": "p","cond": { "$eq":[{"$dateFromString": {"dateString": "$$p.Date","format":"%Y-%m-%d"}},"$init_date"] }}},0]},
-                                   "past_q_spot":{"$arrayElemAt": [{"$filter": {"input": "$prices","as": "p","cond": { "$eq":[{"$dateFromString": {"dateString": "$$p.Date","format":"%Y-%m-%d"}},"$past_q_date"] }}},0]},
-                                   "next_q_spot":{"$arrayElemAt": [{"$filter": {"input": "$prices","as": "p","cond": { "$eq":[{"$dateFromString": {"dateString": "$$p.Date","format":"%Y-%m-%d"}},"$next_q_date"] }}},0]}
-                        }},
-                        {"$project": {"quarter_date":1,"quantity":1,"filer_name":1,"ticker":1,"market_cap":1,"sector":1,"volume":1,
-                                    "q_rel_cap":{"$divide": ["$quantity","$market_cap"]},
-                                    "q_rel_vol":{"$divide": ["$quantity","$volume"]},
-                                    "init_spot":1,"past_q_spot":1,"next_q_spot":1,
-                                    "return_before":{"$add":[{"$divide": ["$init_spot.Close","$past_q_spot.Close" ]},-1]},
-                                    "return_after":{"$add":[{"$divide": ["$next_q_spot.Close","$init_spot.Close" ]},-1]}
-                        }}
-                        ]
 }
 
+materialized_views=[("positions",[{"$addFields": {"sector":{"$concat": [{"$ifNull": [ "$info.sector", "" ]} , {"$ifNull": [ "$info.category", "" ]}]}}},
+                                {"$group": { "_id": {"sector":"$sector","quarter_date":"$quarter_date","filer_name":"$filer_name"},"size":{"$sum":{"$multiply": ["$quantity","$spot"]}},"ticker":{"$sum": 1}}},
+                                {"$project": {"_id":0,"sector":"$_id.sector","quarter_date":"$_id.quarter_date","filer_name":"$_id.filer_name","size":1,"ticker":1}},
+                                { "$merge": { "into": "positions_by_sector","on":"_id", "whenMatched": "replace","whenNotMatched": "insert"  } }])
+                    ]
+
+def refresh_materialized_views(collection,pipeline):
+    logger.info(f'Updating materialized view on: {collection} => {pipeline[-1]["$merge"]["into"]}')
+    db[collection].aggregate(pipeline, allowDiskUse=True)
+    return
+
+
 def create_indices():
+    logger.info('creating indices...')
     filings.create_index([("filer_name",pymongo.ASCENDING),("positions.cusip", pymongo.ASCENDING)])
+    db["positions"].ensure_index([("filer_name",pymongo.ASCENDING),("info.sector",pymongo.ASCENDING),
+                                  ("info.category",pymongo.ASCENDING),("quarter_date",pymongo.ASCENDING)
+                                  ])
 
 
 
 if __name__ == '__main__':
     create_indices()
+    for c,p in materialized_views:
+        refresh_materialized_views(c,p)
     for k, v in views.items():
         create_view(k, v)
+
