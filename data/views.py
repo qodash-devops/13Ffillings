fca37ccf9391448b6446fc412c778249e0f25e3b
karimtaaam <karim.taaam@qodash.com>
2020-03-26 11:13:55 -0400

    remove pycache

--- data/views.py
+++ data/views.py
@@ -40,11 +40,13 @@ views = {
 materialized_views=[("positions",[{"$addFields": {"sector":{"$concat": [{"$ifNull": [ "$info.sector", "" ]} , {"$ifNull": [ "$info.category", "" ]}]}}},
                                 {"$group": { "_id": {"sector":"$sector","quarter_date":"$quarter_date","filer_name":"$filer_name"},"size":{"$sum":{"$multiply": ["$quantity","$spot"]}},"ticker":{"$sum": 1}}},
                                 {"$project": {"_id":0,"sector":"$_id.sector","quarter_date":"$_id.quarter_date","filer_name":"$_id.filer_name","size":1,"ticker":1}},
-                                { "$merge": { "into": "positions_by_sector","on":"_id", "whenMatched": "replace","whenNotMatched": "insert"  } }])
+                                { "$out": "positions_by_sector" }])
                     ]
 
 def refresh_materialized_views(collection,pipeline):
-    logger.info(f'Updating materialized view on: {collection} => {pipeline[-1]["$merge"]["into"]}')
+    output_col=pipeline[-1]["$out"]
+    db[output_col].drop()
+    logger.info(f'Updating materialized view on: {collection} => {output_col}')
     db[collection].aggregate(pipeline, allowDiskUse=True)
     return
 
@@ -52,7 +54,7 @@ def refresh_materialized_views(collection,pipeline):
 def create_indices():
     logger.info('creating indices...')
     filings.create_index([("filer_name",pymongo.ASCENDING),("positions.cusip", pymongo.ASCENDING)])
-    db["positions"].ensure_index([("filer_name",pymongo.ASCENDING),("info.sector",pymongo.ASCENDING),
+    db["positions"].create_index([("filer_name",pymongo.ASCENDING),("info.sector",pymongo.ASCENDING),
                                   ("info.category",pymongo.ASCENDING),("quarter_date",pymongo.ASCENDING)])
 
 
